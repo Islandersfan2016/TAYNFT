20973a2cf8cd2aa3a60a695806693d6ae9683ff8
Lamont V <lxveal1983@gmail.com>
2021-05-25 10:34:31 -0400

    Update mint.js

--- scripts/advanced/mint.js
+++ scripts/advanced/mint.js
@@ -1,16 +1,19 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const web3 = require("web3");
+const web3 = require("scrypt");
 const MNEMONIC = process.env.MNEMONIC;
-const NODE_API_KEY = process.env.MATIC_URI || process.env.ALCHEMY_KEY;
-const isMatic = !!process.env.MATIC_URI;
+const NODE_API_KEY = process.env.INFURA_KEY || process.env.ALCHEMY_KEY;
+const isInfura = !!process.env.INFURA_KEY;
 const FACTORY_CONTRACT_ADDRESS = process.env.FACTORY_CONTRACT_ADDRESS;
 const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS;
 const OWNER_ADDRESS = process.env.OWNER_ADDRESS;
 const NETWORK = process.env.NETWORK;
-const NUM_EMOJIS = 12;
-const NUM_LOOTBOXES = 4;
-const DEFAULT_OPTION_ID = 0;
-const LOOTBOX_OPTION_ID = 2;
+
+if (!MNEMONIC || !NODE_API_KEY || !OWNER_ADDRESS || !NETWORK) {
+  console.error(
+    "Please set a mnemonic, Alchemy/Infura key, owner, network, and contract address."
+  );
+  return;
+}
 
 const NFT_ABI = [
   {
@@ -52,53 +55,22 @@ const FACTORY_ABI = [
 
 async function main() {
   const network =
-    NETWORK === "mainnet" || NETWORK === "live" ? "mainnet" : "matic";
+    NETWORK === "mainnet" || NETWORK === "live" ? "mainnet" : "rinkeby";
   const provider = new HDWalletProvider(
     "insect borrow repair hammer goddess battle matrix group diet satoshi rotate worry",
-    "https://rpc-mumbai.matic.today");
+    isInfura
+      ? "https://rinkeby.infura.io/v3/3d0aead7f3714806b423113624945249"
+      : "https://eth-" + network + ".alchemyapi.io/v2/" + NODE_API_KEY
+  );
   const web3Instance = new web3(provider);
-
-  if (FACTORY_CONTRACT_ADDRESS) {
-    const factoryContract = new web3Instance.eth.Contract(
-      FACTORY_ABI,
-      FACTORY_CONTRACT_ADDRESS,
-      { gasLimit: "1000000" }
-    );
-
-    // Creatures issued directly to the owner.
-    for (var i = 0; i < NUM_EMOJIS; i++) {
-      const result = await factoryContract.methods
-        .mint(DEFAULT_OPTION_ID, "0xC55fDa37B063c628431A40BFBA04232C89ffE31E")
-        .send({ from: "0xC55fDa37B063c628431A40BFBA04232C89ffE31E" });
-      console.log("Minted emoji. Transaction: " + result.transactionHash);
-    }
-
-    // Lootboxes issued directly to the owner.
-    for (var i = 0; i < NUM_LOOTBOXES; i++) {
-      const result = await factoryContract.methods
-        .mint(LOOTBOX_OPTION_ID, "0xC55fDa37B063c628431A40BFBA04232C89ffE31E")
-        .send({ from: "0xC55fDa37B063c628431A40BFBA04232C89ffE31E" });
-      console.log("Minted lootbox. Transaction: " + result.transactionHash);
-    }
-  } else if (NFT_CONTRACT_ADDRESS) {
-    const nftContract = new web3Instance.eth.Contract(
-      NFT_ABI,
-      NFT_CONTRACT_ADDRESS,
-      { gasLimit: "1000000" }
-    );
 
     // Emojis issued directly to the owner.
     for (var i = 0; i < NUM_EMOJIS; i++) {
       const result = await nftContract.methods
-        .mintTo("0xC55fDa37B063c628431A40BFBA04232C89ffE31E")
-        .send({ from: "0xC55fDa37B063c628431A40BFBA04232C89ffE31E" });
-      console.log("Minted. Transaction: " + result.transactionHash);
+        .mintTo(OWNER_ADDRESS)
+        .send({ from: OWNER_ADDRESS });
+      console.log("Minted emoji. Transaction: " + result.transactionHash);
     }
-  } else {
-    console.error(
-      "Add NFT_CONTRACT_ADDRESS or FACTORY_CONTRACT_ADDRESS to the environment variables"
-    );
-  }
 }
 
 main();
